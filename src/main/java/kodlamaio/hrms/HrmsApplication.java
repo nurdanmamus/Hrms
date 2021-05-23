33a7c9bff1ebc58969bb8c3edc544a7d3fe32873
nurdan <nurdan@DESKTOP-A3RNKRN>
2021-05-23 15:43:04 +0300

    Hrms

--- src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -2,12 +2,32 @@ package kodlamaio.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HrmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
+	
+	//@EnableSwagger2 swagger'ı başlatıyor. Docket üzeerinden apileri buluyır ve documente haline getiriyor.
+		//config için kullanıyor. @bean 'i görünce belleğe yerleştiriyor.Docket ile controllerlarlarda
+		//request handleları bir dokümante haline getiriyor. bütün apileri bulup test edilebilir hale getiriyor.
+		@Bean
+	    public Docket api() { 
+	        return new Docket(DocumentationType.SWAGGER_2)  
+	          .select()                                  
+	          .apis(RequestHandlerSelectors.basePackage("kodlamaio.hrms"))              
+	          .paths(PathSelectors.any())                          
+	          .build();                                           
+	    }
 
 }
--- src/main/java/kodlamaio/hrms/api/controllers/PositionsController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/PositionsController.java
@@ -10,24 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import kodlamaio.hrms.business.abstracts.PositionService;
 import kodlamaio.hrms.entities.concretes.Position;
 
-
 @RestController
 @RequestMapping("/api/positions")
 public class PositionsController {
-	
+
 	private PositionService positionService;
-	
+
 	@Autowired
 	public PositionsController(PositionService positionService) {
 		super();
 		this.positionService = positionService;
 	}
 
-		//kodlama.oi/api/positions/getall -> böyle bir istek gelirse bu class karşılayacak
-		@GetMapping("/getall")
-		public List<Position> getAll(){
-			//api-> bussiness-> dataAccedd
-			return this.positionService.getAll();
-		}
+	// kodlama.oi/api/positions/getall -> böyle bir istek gelirse bu class
+	// karşılayacak
+	@GetMapping("/getall")
+	public List<Position> getAll() {
+		// api-> bussiness-> dataAccedd
+		return this.positionService.getAll();
+	}
 
 }
